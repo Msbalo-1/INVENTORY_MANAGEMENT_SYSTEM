305482d7ae110b0a52e6be7183aa51928ce404a2
Msbalo-1 <mustaphabalogun860@gmail.com>
2023-03-08 22:56:18 +0100

    Update

--- inventory/models.py
+++ inventory/models.py
@@ -2,5 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Catigories(models.Model):
-  # name = models.CharField(max_length=250)
+  # name = models.
+  date_created = models.DateTimeField(auto_now_add=True)
+  date_updated = models.DateTimeField(auto_now=True)
+  pass
+
+
+class Product(models.Model):
+  # catigorie = models.ForeignKey(Catigories, null=True, blank=True, on_delete=models.SET_NULL)
+  catigorie = models.ManyToManyField('Tag', blank=True)
+  # name =
   pass
